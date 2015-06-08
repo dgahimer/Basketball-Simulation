28c27d748c190dc04966ff646030d368939ee39b
Dustin Gahimer <dgahimer@gmail.com>
2015-06-08 18:41:24 -0500

    Adding more barebones rulesets

---
+++ Basketball-Simulation/Basketball-Simulation/League/LeagueRules.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basketball_Simulation.League
+{
+    public class LeagueRules
+    {
+        // TODO: Add salary cap rules
+        // TODO: Add draft rules, recruiting rules, etc.
+    }
+}
---
+++ Basketball-Simulation/Basketball-Simulation/Season/SeasonRules.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basketball_Simulation.Season
+{
+    public class SeasonRules
+    {
+        public byte PreseasonGames { get; set; } // Number of preseason games per team
+        public byte RegularSeasonGames { get; set; } // Number of regular season games per team
+
+        public byte PlayoffRounds { get; set; } // Number of rounds in the playoffs
+        // TODO: Figure out a way to allow customization between round robin and head-to-head
+        // TODO: Figure out a way to keep track of which teams get how many byes
+    }
+}
