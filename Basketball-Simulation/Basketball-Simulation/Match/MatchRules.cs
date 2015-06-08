1d35d5bded7a9962ff0e11aa091c6dc2f55bd934
Dustin Gahimer <dgahimer@gmail.com>
2015-06-08 18:11:32 -0500

    Adding Solution & Match Rules

---
+++ Basketball-Simulation/Basketball-Simulation/Match/MatchRules.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basketball_Simulation.Match
+{
+    // Contains all the pertinant rules for a match
+    public class MatchRules
+    {
+        public enum OvertimeType
+        {
+            None, // Draw after regulation
+            Single, // Single overtime period, then draw
+            Continuous // Overtime until a winner is decided
+        };
+
+        public int RegulationPeriod { get; set; } // Number of regulation periods
+        public int RegulationPeriodLength { get; set; } // Length of regulation periods (in seconds)
+
+        public OvertimeType Overtime { get; set; } // Overtime type for this game
+        public int OvertimeLength { get; set; } // Length of each overtime period
+    }
+}
